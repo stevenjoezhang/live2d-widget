2a3be9af2a4bd658794feeb6b779e61f03f2eda6
冯国清 <master@rebeta.cn>
2019-09-30 19:35:29 +0800

    url

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 //注意：live2d_path参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";
-//const live2d_path = "/live2d-widget/";
+//const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";
+const live2d_path = "/js/live2d-widget/";
 
 //加载waifu.css
 $("<link>").attr({ href: live2d_path + "waifu.css", rel: "stylesheet" }).appendTo("head");
