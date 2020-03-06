b17e3dff7505925e6160e267f0424fde36178d50
liuxiaoppppp <49995029+liuxiaoppppp@users.noreply.github.com>
2020-03-06 23:11:20 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -34,7 +34,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/liuxiaoppppp/live2d_api/"
+		//	cdnPath: "https://cdn.jsdelivr.net/gh/liuxiaoppppp/live2d_api/"
 		});
 	});
 }
