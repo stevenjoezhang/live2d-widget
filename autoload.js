c4c685ac182691122b118adb99321b0253ee57f9
liuxiaoppppp <49995029+liuxiaoppppp@users.noreply.github.com>
2020-03-09 00:22:39 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,5 +1,4 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/liuxiaoppppp/live2d-widget@latest/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -33,7 +32,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
+			apiPath: "https://cdn.jsdelivr.net/gh/liuxiaoppppp/live2d_api/",
 			cdnPath: "https://cdn.jsdelivr.net/gh/liuxiaoppppp/live2d_api/"
 		});
 	});
