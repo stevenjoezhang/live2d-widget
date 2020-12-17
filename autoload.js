62826c521e80bc275fc24301bcd858f8a4dea9b8
nonsoft-dev <nonsoft.dev@gmail.com>
2020-12-17 02:31:49 +0000

    update cdn

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/nonsoft-dev/live2d-widget@latest/";
-//const live2d_path = "/live2d-widget/";
+// const live2d_path = "https://cdn.jsdelivr.net/gh/nonsoft-dev/live2d-widget@latest/";
+const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
