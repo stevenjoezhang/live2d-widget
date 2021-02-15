c946d34118c5101a232e7b2bbed8711408ecef8c
KnowsCount <knowscount@gmail.com>
2021-02-15 20:26:15 +0800

    bug fixes?

--- autoload.js
+++ autoload.js
@@ -1,44 +1,50 @@
+/*
+ * @Date: 15/02/2021 16.05.35 +0800
+ * @Author: KnowsCount
+ * @LastEditTime: 15/02/2021 20.21.07 +0800
+ * @FilePath: /@knowscount:live2d-widget/autoload.js
+ */
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/PFiS1737/live2d-widget@latest/";
+const live2d_path = 'https://cdn.jsdelivr.net/gh/PFiS1737/live2d-widget@latest/'
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
 	return new Promise((resolve, reject) => {
-		let tag;
+		let tag
 
-		if (type === "css") {
-			tag = document.createElement("link");
-			tag.rel = "stylesheet";
-			tag.href = url;
-			tag.id = "live2d_css";
-		}
-		else if (type === "js") {
-			tag = document.createElement("script");
-			tag.src = url;
+		if (type === 'css') {
+			tag = document.createElement('link')
+			tag.rel = 'stylesheet'
+			tag.href = url
+			tag.id = 'live2d_css'
+		} else if (type === 'js') {
+			tag = document.createElement('script')
+			tag.src = url
 		}
 		if (tag) {
-			tag.onload = () => resolve(url);
-			tag.onerror = () => reject(url);
-			document.head.appendChild(tag);
+			tag.onload = () => resolve(url)
+			tag.onerror = () => reject(url)
+			document.head.appendChild(tag)
 		}
-	});
+	})
 }
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
-if (screen.width >= 768) {
-	// "768"为最低显示宽度，单位为像素
-	Promise.all([
-		loadExternalResource(live2d_path + "waifu_left.css", "css"),
-		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
-	]).then(() => {
-		initWidget({
-			waifuPath: live2d_path + "waifu-tips.json",
-			// apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
-		});
-	});
-}
+//if (screen.width >= 768) {
+// "768"为最低显示宽度，单位为像素
+Promise.all([
+	loadExternalResource(live2d_path + 'waifu_left.css', 'css'),
+	loadExternalResource(live2d_path + 'live2d.min.js', 'js'),
+	loadExternalResource(live2d_path + 'waifu-tips.js', 'js'),
+]).then(() => {
+	initWidget({
+		waifuPath: live2d_path + 'waifu-tips.json',
+		// apiPath: "https://live2d.fghrsh.net/api/",
+		cdnPath: 'https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/',
+	})
+})
+//}
+
 // initWidget 第一个参数为 waifu-tips.json 的路径，第二个参数为 API 地址
 // API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
 // 初始化看板娘会自动加载指定目录下的 waifu-tips.json
@@ -61,4 +67,4 @@ console.log(`
               ﾚ'ヽL__|___i,___,ンﾚ|ノ
                   ﾄ-,/  |___./
                   'ｰ'    !_,.:
-`);
+`)
