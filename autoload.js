d598ff939b6730df244e91ea0ef2867a4f429cee
pymastera <89636508+pymastera@users.noreply.github.com>
2022-05-27 13:37:13 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -28,7 +28,7 @@ function loadExternalResource(url, type) {
 if (screen.width >= 768) {
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.css", "css"),
-		loadExternalResource(live2d_path + "drag.js", "js")
+		loadExternalResource(live2d_path + "drag.js", "js"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js"),
 		
