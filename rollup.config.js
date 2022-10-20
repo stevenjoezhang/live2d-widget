abb832a9ea3f006b7d8b9c0567bba99c5603b426
Mimi <1119186082@qq.com>
2022-10-20 17:28:52 +0800

    Inline SVG icon

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { createFilter } from "@rollup/pluginutils";
+
+function string(opts = {}) {
+    if (!opts.include) {
+        throw Error("include option should be specified");
+    }
+
+    const filter = createFilter(opts.include, opts.exclude);
+
+    return {
+        name: "string",
+
+        transform(code, id) {
+            if (filter(id)) {
+                return {
+                    code: `export default ${JSON.stringify(code)};`,
+                    map: { mappings: "" }
+                };
+            }
+        }
+    };
+}
+
+export default {
+    input: 'src/waifu-tips.js',
+    plugins: [nodeResolve(), string({
+        include: "**/*.svg",
+    })]
+};
