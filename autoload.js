6cb23799210953eb4ccad2d6560a53a8305976ab
GAOYHJ <gaoyhj@digitalchina.com>
2022-10-28 16:16:52 +0800

    修改

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 // live2d_path 参数建议使用绝对路径
 const live2d_path =
-    "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
+    "https://fastly.jsdelivr.net/gh/xiaoheihahah/live2d-widget@latest/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
