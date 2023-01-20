d199c2642281783788df4cf791cbdcc2f918125f
Zijian Zhou <86515498+Garrukzijian@users.noreply.github.com>
2023-01-20 20:57:33 +0700

    1

--- autoload.js
+++ autoload.js
@@ -36,7 +36,7 @@ if (screen.width >= 768) {
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
-			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
+			tools: []
 		});
 	});
 }
