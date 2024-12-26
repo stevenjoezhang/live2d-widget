5e14e1c13b435a82499bef5e683f7501e4f16a94
Mimi <1119186082@qq.com>
2024-12-26 23:39:12 +0800

    Update package path

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
+const live2d_path = "https://fastly.jsdelivr.net/npm/live2d-widgets@0/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
