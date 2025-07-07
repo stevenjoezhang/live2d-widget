2e5999ca2b881be4372b1f1410b5b9c26345a128
Mimi <1119186082@qq.com>
2025-07-07 11:23:24 +0800

    Release 1.0.0-rc.5

--- dist/autoload.js
+++ dist/autoload.js
@@ -5,7 +5,7 @@
 
 // Recommended to use absolute path for live2d_path parameter
 // live2d_path 参数建议使用绝对路径
-const live2d_path = 'https://fastly.jsdelivr.net/npm/live2d-widgets@1.0.0-rc.4/dist/';
+const live2d_path = 'https://fastly.jsdelivr.net/npm/live2d-widgets@1.0.0-rc.5/dist/';
 // const live2d_path = '/dist/';
 
 // Method to encapsulate asynchronous resource loading
