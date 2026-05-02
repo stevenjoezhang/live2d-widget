9519f5469c9086359042f3ffd2d965a0fc3e4162
Mercury <505350617@qq.com>
2026-05-02 15:02:41 +0800

    Update autoload.js

--- dist/autoload.js
+++ dist/autoload.js
@@ -5,7 +5,7 @@
 
 // Recommended to use absolute path for live2d_path parameter
 // live2d_path 参数建议使用绝对路径
-const live2d_path = 'https://cdn.jsdelivr.net/gh/Shi-xiaotong/live2d-widget@latest/';
+const live2d_path = 'https://cdn.jsdelivr.net/gh/Shi-xiaotong/live2d-widget@latest/dist/';
 // const live2d_path = '/dist/';
 
 // Method to encapsulate asynchronous resource loading
